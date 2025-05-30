76531e0881e53b59493d539370e8496ba8b207ed
Jason Burke <jpburke77@gmail.com>
2025-05-30 17:48:31 -0500

    task: complete 1.3-1.5 exercises

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,12 +1,5 @@
-const course = 'Half Stack application development';
-const courseData = [
-  { part1: 'Fundamentals of React', exercise1: 10 },
-  { part2: 'Using props to pass data', exercise2: 7 },
-  { part3: 'State of a component', exercise3: 14 },
-];
-
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.course.name}</h1>;
 };
 
 const Part = (data) => {
@@ -20,9 +13,18 @@ const Part = (data) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props[0].part1} exercise={props[0].exercise1} />
-      <Part part={props[1].part2} exercise={props[1].exercise2} />
-      <Part part={props[2].part3} exercise={props[2].exercise3} />
+      <Part
+        part={props.course.parts[0].name}
+        exercise={props.course.parts[0].exercises}
+      />
+      <Part
+        part={props.course.parts[1].name}
+        exercise={props.course.parts[1].exercises}
+      />
+      <Part
+        part={props.course.parts[2].name}
+        exercise={props.course.parts[2].exercises}
+      />
     </>
   );
 };
@@ -31,17 +33,37 @@ const Total = (props) => {
   return (
     <p>
       Number of exercises{' '}
-      {props[0].exercise1 + props[1].exercise2 + props[2].exercise3}
+      {props.course.parts[0].exercises +
+        props.course.parts[1].exercises +
+        props.course.parts[2].exercises}
     </p>
   );
 };
 
 const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+
   return (
     <div>
       <Header course={course} />
-      <Content {...courseData} />
-      <Total {...courseData} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
