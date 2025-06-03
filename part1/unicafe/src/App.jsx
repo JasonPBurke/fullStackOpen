afef97c0d06c89d1d8ffa9e12633f65fb94de1fc
Jason Burke <jpburke77@gmail.com>
2025-06-03 18:53:22 -0500

    fix: actually complete exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react';
 
+const Heading = ({ text }) => <h1>{text}</h1>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const setRating = (value, setValue) => () => setValue(value + 1);
+
+  return (
+    <>
+      <Heading text='give feedback' />
+      <Button onClick={setRating(good, setGood)} text='good'></Button>
+      <Button onClick={setRating(neutral, setNeutral)} text='neutral'></Button>
+      <Button onClick={setRating(bad, setBad)} text='bad'></Button>
+      <Heading text='statistics' />
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  );
 };
 
 export default App;
