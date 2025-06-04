3a28a556a847cf91a1ffcb15607e46ff4d7aed53
Jason Burke <jpburke77@gmail.com>
2025-06-03 19:27:32 -0500

    chore: complete exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,6 +4,14 @@ const Heading = ({ text }) => <h1>{text}</h1>;
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
+const Statistics = ({ name, value }) => {
+  return (
+    <div>
+      {name} {value}
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -11,26 +19,22 @@ const App = () => {
   const [bad, setBad] = useState(0);
 
   const setRating = (value, setValue) => () => setValue(value + 1);
-
-  const calculateAverage = () => {
-    return (good * 1 + bad * -1) / (good + neutral + bad);
-  };
-
-  calculateAverage();
+  const calculateAverage = () => (good * 1 + bad * -1) / (good + neutral + bad);
+  const calculatePositive = () => (good / (good + bad + neutral)) * 100 + '%';
 
   return (
     <>
-      <Heading text='give feedback' />
+      <Heading text='give feedback'></Heading>
       <Button onClick={setRating(good, setGood)} text='good'></Button>
       <Button onClick={setRating(neutral, setNeutral)} text='neutral'></Button>
       <Button onClick={setRating(bad, setBad)} text='bad'></Button>
-      <Heading text='statistics' />
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {good + neutral + bad}</div>
-      <div>average {calculateAverage()}</div>
-      <div>positive {(good / (good + neutral + bad)) * 100}%</div>
+      <Heading text='statistics'></Heading>
+      <Statistics name='good' value={good}></Statistics>
+      <Statistics name='neutral' value={neutral}></Statistics>
+      <Statistics name='bad' value={bad}></Statistics>
+      <Statistics name='all' value={good + neutral + bad}></Statistics>
+      <Statistics name='average' value={calculateAverage()}></Statistics>
+      <Statistics name='positive' value={calculatePositive()}></Statistics>
     </>
   );
 };
